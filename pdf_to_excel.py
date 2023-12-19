beec79506c61572ab787a257ac92d32708b674f8
Andrew Pipo <apipo@atechtraining.com>
2023-12-19 13:25:32 -0500

    Adjusted to have user select folder for both excel and/or text files

--- pdf_to_excel.py
+++ pdf_to_excel.py
@@ -33,58 +33,68 @@ def pdf_to_excel():
 
     # Open a file dialog to select the PDF file
     pdf_file = filedialog.askopenfilename(title="Select the PDF file", filetypes=[("PDF Files", "*.pdf")])
-    # Open a file dialog to select the output Excel file
-    excel_file = filedialog.asksaveasfilename(title="Select or name and save a new Excel file", defaultextension=".xlsx", filetypes=[("Excel Files", "*.xlsx")])
 
-    # Obtain the pdf name
+    # obtain the pdf file name
     pdf_file_name = os.path.splitext(os.path.basename(pdf_file))[0]
+
+
+    # Open a file dialog to select the folder for output files
+    folder_selected = filedialog.askdirectory(title="Select folder location for output files")
+
+    # Obtain the path for the excel file
+    excel_file = os.path.join(folder_selected, "output.xlsx")
 
     # Ask the user to input the page numbers
     pages = input("Enter the page numbers separated by commas (e.g., 1,2,3): ")
     pages = list(map(int, pages.split(',')))
 
-    # Create a Pandas Excel writer using XlsxWriter as the engine
-    writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
-
     # Open the PDF file
     with pdfplumber.open(pdf_file) as pdf:
-        # Create a Pandas Excel writer using XlsxWriter as the engine
-        with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
-            for i, p in enumerate(pages):
-                page = pdf.pages[p-1]  # pdfplumber uses zero-based page indexing
-                tables = page.extract_tables()  # extract all tables into a list of tables
-                if any(cell.strip() for table in tables for row in table for cell in row):  # check if there are any non-empty tables that exist
-                    for j, table in enumerate(tables):
-                        df = pd.DataFrame(table[1:], columns=table[0])  # convert each table into a pandas DataFrame
+        # Check if there are any tables on the specified pages
+        has_tables = False
+        for p in pages:
+            page = pdf.pages[p-1]  # pdfplumber uses zero-based page indexing
+            tables = page.extract_tables()  # extract all tables into a list of tables
+            if any(cell.strip() for table in tables for row in table for cell in row):  # check if there are any non-empty tables that exist
+                has_tables = True
+                break
 
-                        # format to exclude rows that have a reserved name or is '-'
-                        df = df[df[df.columns[1]] != 'Reserved']
-                        df = df[df[df.columns[1]] != '—']
+        # If there are tables, create the Excel file and write the tables to it
+        if has_tables:
+            with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
+                for i, p in enumerate(pages):
+                    page = pdf.pages[p-1]  # pdfplumber uses zero-based page indexing
+                    tables = page.extract_tables()  # extract all tables into a list of tables
+                    if any(cell.strip() for table in tables for row in table for cell in row):  # check if there are any non-empty tables that exist
+                        for j, table in enumerate(tables):
+                            df = pd.DataFrame(table[1:], columns=table[0])  # convert each table into a pandas DataFrame
 
-                        # sets the data to numeric if it reads a numeric value as a string, otherwise leave alone
-                        for col in df.columns:
-                            df[col] = df[col].apply(lambda x: pd.to_numeric(x, errors='ignore') if re.search(r'\d', str(x)) else x)
+                            # format to exclude rows that have a reserved name or is '-'
+                            df = df[df[df.columns[1]] != 'Reserved']
+                            df = df[df[df.columns[1]] != '—']
 
-                        # write each DataFrame to the Excel file
-                        df.to_excel(writer, sheet_name = pdf_file_name[:10] + '_Page' + str(p) + "_" + str(i+1) + '_Table' + str(j+1), index=False)
-                else:
-                    break
-        # if no tables exist, write the text
-        pdf_to_txt(pdf_file, pages)
+                            # sets the data to numeric if it reads a numeric value as a string, otherwise leave alone
+                            for col in df.columns:
+                                df[col] = df[col].apply(lambda x: pd.to_numeric(x, errors='ignore') if re.search(r'\d', str(x)) else x)
 
+                            # write each DataFrame to the Excel file
+                            df.to_excel(writer, sheet_name = pdf_file_name[:10] + '_Page' + str(p) + '_Table' + str(j+1), index=False)
+        else:
+            # if no tables exist, write the text
+            pdf_to_txt(pdf_file, folder_selected, pages)
 
 
 '''
 pdf_to_txt(pdf_file, pages) is a function that reads the text of the pdf and converts into a .txt file
 if no table exists. pdf_file is a string param and pages is a list[int] param
 '''
-def pdf_to_txt(pdf_file, pages):
+def pdf_to_txt(pdf_file, folder_selected, pages):
     # Create a root window and hide it
     root = tk.Tk()
     root.withdraw()
 
     # Open a file dialog to select the output text file
-    txt_file = filedialog.asksaveasfilename(title="Name and save the output text file", defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
+    txt_file = os.path.join(folder_selected, "output.txt")
 
     # Ask the user for the start and end page numbers
     start_page = min(pages)-1
